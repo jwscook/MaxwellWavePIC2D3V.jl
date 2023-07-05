dc87f7488b9c98245c27c14643db7010def1dbb6
James Cook <james.cook@ukaea.uk>
2023-07-05 06:57:27 +0100

    updated examples

--- examples/RingBeam.jl
+++ examples/RingBeam.jl
@@ -20,7 +20,7 @@ function pic()
   MU_0 = 4.0e-7 * π;
   EPSILON_0 = 1.0 / MU_0 / SPEED_OF_LIGHT / SPEED_OF_LIGHT;
 
-  n = 1e19
+  n = 1e20
   B = 2.1
   Mi = 64 #2 * 1836
   Me = 1
@@ -31,7 +31,7 @@ function pic()
   vthe = sqrt(TeeV * ELEMENTARY_CHARGE * 2 / Me / ELEMENTARY_MASS)
   lD0 = vthe / Wp
   Ωi0 = ELEMENTARY_CHARGE * B / Mi / ELEMENTARY_MASS
-  kresolution = 5
+  kresolution = 1
   L = Va / Ωi0 * 2π * kresolution
   @show REQUIRED_GRID_CELLS = L / lD0
 
@@ -58,63 +58,40 @@ function pic()
   n0 = n / m_numberDensityScale
   Πe = sqrt(ELEMENTARY_CHARGE^2 * n / EPSILON_0 / Me / ELEMENTARY_MASS) * m_timeScale
   vth = sqrt(TeeV * ELEMENTARY_CHARGE * 2 / Me / ELEMENTARY_MASS) / m_lengthScale * m_timeScale
-  Ωi = B0 / Mi
-  #Ωi, Ωi * m_timeScale
-  ld = vth / Πe
-  #lD0 / m_lengthScale, ld
   Va = Va / SPEED_OF_LIGHT
 
-  #@timeit to "Initialisation" begin
-    Lx = L0
-    Ly = Lx * NY / NX
-    dt = Lx / NX / 8
-    P = NX * NY * 8
-    NT = 2^16 #2^10#2^14
-    Δx = Lx / NX
-    Δx = Lx / NX
-    Δy = Ly / NY
-    dl = min(Lx / NX, Ly / NY)
-    #n0 = 3.5e6 #4 * pi^2
-    #debyeoverresolution = 1
-    #vth = 0.01 #debyeoverresolution * dl * sqrt(n0)
-    #B0 = sqrt(n0) / 4;
-    #λ = vth / sqrt(n0)
-    #rL = vth / B0 = 4λ
+  Ωi = B0 / Mi
+  ld = vth / Πe
 
-    #dt = dl/6vth
-    #field = MaxwellWavePIC2D3V.ElectrostaticField(NX, NY, Lx, Ly, dt=dt, B0x=B0)
-    #diagnostics = MaxwellWavePIC2D3V.ElectrostaticDiagnostics(NX, NY, NT, ntskip, 2)
-    ntskip = 16 #4#prevpow(2, round(Int, 10 / 6vth)) ÷ 4
-    ngskip = 4
-    @show NT ÷ ntskip
-    #dt = 2dl #/6vth
-    #dt = dl / vth
-    field = MaxwellWavePIC2D3V.LorenzGaugeField(NX, NY, Lx, Ly, dt=dt, B0y=B0,
-      imex=MaxwellWavePIC2D3V.ImEx(1), buffer=10)
-    #field = MaxwellWavePIC2D3V.LorenzGaugeStaggeredField(NX, NY, Lx, Ly, dt=dt, B0z=B0,
-    #  imex=MaxwellWavePIC2D3V.ImEx(1), buffer=10)
-    #field = MaxwellWavePIC2D3V.LorenzGaugeSemiImplicitField(NX, NY, Lx, Ly, dt=dt, B0x=B0,
-    #  fieldimex=MaxwellWavePIC2D3V.ImEx(1.0), sourceimex=MaxwellWavePIC2D3V.ImEx(0.05),
-    #  buffer=10, rtol=sqrt(eps()), maxiters=1000)
-    diagnostics = MaxwellWavePIC2D3V.LorenzGaugeDiagnostics(NX, NY, NT, ntskip, ngskip; makegifs=false)
-    shape = MaxwellWavePIC2D3V.BSplineWeighting{@stat 5}()
-    #shape = MaxwellWavePIC2D3V.NGPWeighting();#
-    #shape = MaxwellWavePIC2D3V.AreaWeighting();#
-    electrons = MaxwellWavePIC2D3V.Species(P, vth, n0, shape;
-      Lx=Lx, Ly=Ly, charge=-1, mass=Me)
-    ions = MaxwellWavePIC2D3V.Species(P, vth / sqrt(Mi / Me), n0, shape;
-      Lx=Lx, Ly=Ly, charge=1, mass=Mi)
-    sort!(electrons, Lx / NX, Ly / NY)
-    sort!(ions, Lx / NX, Ly / NY)
-    plasma = [electrons, ions]
-    #@show NX, NY, P, NT, NT÷ntskip, ntskip, dl, n0, vth, B0, dt
-    #@show vth * (NT * dt)
-    #@show (NT * dt) / (2pi/B0), (2pi/B0) / (dt * ntskip)
-    #@show (NT * dt) / (2pi/sqrt(n0)),  (2pi/sqrt(n0)) / (dt * ntskip)
-#  end
+  Lx = L0
+  Ly = Lx * NY / NX
+  dt = Lx / NX / 8
+  P = NX * NY * 8
+  NT = 2^14
+  Δx = Lx / NX
+  Δx = Lx / NX
+  Δy = Ly / NY
+  dl = min(Lx / NX, Ly / NY)
+
+  ntskip = 16
+  ngskip = 4
+  @show NT ÷ ntskip
+  field = MaxwellWavePIC2D3V.LorenzGaugeField(NX, NY, Lx, Ly, dt=dt, B0y=B0,
+    imex=MaxwellWavePIC2D3V.ImEx(1), buffer=10)
+  #field = MaxwellWavePIC2D3V.LorenzGaugeSemiImplicitField(NX, NY, Lx, Ly, dt=dt, B0x=B0,
+  #  fieldimex=MaxwellWavePIC2D3V.ImEx(1.0), sourceimex=MaxwellWavePIC2D3V.ImEx(0.05),
+  #  buffer=10, rtol=sqrt(eps()), maxiters=1000)
+  diagnostics = MaxwellWavePIC2D3V.LorenzGaugeDiagnostics(NX, NY, NT, ntskip, ngskip; makegifs=false)
+  shape = MaxwellWavePIC2D3V.BSplineWeighting{@stat 5}()
+  electrons = MaxwellWavePIC2D3V.Species(P, vth, n0, shape;
+    Lx=Lx, Ly=Ly, charge=-1, mass=Me)
+  ions = MaxwellWavePIC2D3V.Species(P, vth / sqrt(Mi / Me), n0, shape;
+    Lx=Lx, Ly=Ly, charge=1, mass=Mi)
+  sort!(electrons, Lx / NX, Ly / NY)
+  sort!(ions, Lx / NX, Ly / NY)
+  plasma = [electrons, ions]
 
   MaxwellWavePIC2D3V.printresolutions(plasma, field, dt, NT, to)
-  #MaxwellWavePIC2D3V.warmup!(field, plasma, to)
   for t in 0:NT-1;
     MaxwellWavePIC2D3V.loop!(plasma, field, to, t)
     MaxwellWavePIC2D3V.diagnose!(diagnostics, field, plasma, t, to)
